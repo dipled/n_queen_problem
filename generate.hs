1926ec53065b815f88a9f2626d5f6fc9f2870c4d
helena <helenavargastannuri@gmail.com>
2024-09-28 11:27:51 -0300

    talvez eu seja a senhora haskell mesmo

--- generate.hs
+++ generate.hs
@@ -1,34 +1,54 @@
+queens :: Int
+queens = 5
 
-queens = 4
-
+atomify :: (Show a, Show b) => (a, b) -> String
 atomify (x, y) = "q" ++ show x ++ show y
 
+andify :: (Show a, Show b, Show c, Show d) => (a, b) -> (c, d) -> String
 andify a b = "(and " ++ atomify a ++ " " ++ atomify b ++ ")"
 
+getCons :: (Show a, Show b) => [(a, b)] -> String
 getCons l = concat (map (\(a, b) -> "(declare-const q" ++ show a ++ show b ++ " Bool)\n") l)
 
-minLine l = "(assert (or" ++ concat (map (\e -> " " ++ atomify e) l) ++ "))"
+minCell :: (Show a, Show b) => [(a, b)] -> String
+minCell l = "(assert (or" ++ concat (map (\e -> " " ++ atomify e) l) ++ "))"
 
-maxLine l ll n = "(assert (not (or" ++ go l ll n
-    where
-        go l [] n = "))))\n"
-        go l (x : []) n = go (tail l) (tail l) (n - 1)
-        go l (x : y : xs) n = " " ++ andify x y ++ go l (x : xs) n
+minCells :: (Show a, Show b) => [[(a, b)]] -> String
+minCells [] = []
+minCells l@(x : xs) = minCell x ++ "\n" ++ minCells xs
 
-maxLines l n = concat $  map (\e -> maxLine e e n) (take n $ liner n l)
+maxCell :: (Show a, Show b) => [(a, b)] -> [(a, b)] -> Int -> String
+maxCell l ll n = "(assert (not (or" ++ go l ll n
+  where
+    go l [] n = ")))\n"
+    go l (x : []) n = go (tail l) (tail l) (n - 1)
+    go l (x : y : xs) n = " " ++ andify x y ++ go l (x : xs) n
 
-minLines [] = []
-minLines l@(x : xs) = minLine x ++ "\n" ++ minLines xs
+maxCells :: (Show a, Show b) => [(a, b)] -> Int -> String
+maxCells l n = concat $ map (\e -> maxCell e e n) (take n $ divider n l)
 
-liner n [] = []
-liner n l = take n l : liner n (drop n l)
+divider :: Int -> [a] -> [[a]]
+divider n [] = []
+divider n l = take n l : divider n (drop n l)
 
-numbers = [(i,j) | i <- [0..queens - 1], j <- [0..queens - 1]]
+cellLines :: [(Int, Int)]
+cellLines = [(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1]]
 
+cellColumns :: [(Int, Int)]
+cellColumns = [(j, i) | i <- [0 .. queens - 1], j <- [0 .. queens - 1]]
 
+diagonal :: [[(Int, Int)]]
+diagonal = [[(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1], i - j == k] | k <- [-queens + 2 .. queens - 2]]
+
+antidiagonal :: [[(Int, Int)]]
+antidiagonal = [[(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1], i + j == k] | k <- [1 .. 2 * queens - 3]]
+
+main :: IO ()
 main = do
-
-    putStrLn $ getCons (numbers)
-    putStrLn $ minLines $ liner queens numbers
-    putStrLn $ maxLines numbers queens
-
+   let s1 = ";constantes\n" ++ getCons cellLines
+   let s2 = ";cada linha possui ao menos uma rainha\n" ++ minCells (divider queens cellLines)
+   let s3 = ";cada linha possui no máximo uma rainha\n" ++ maxCells cellLines queens
+   let s4 = ";cada coluna possui no máximo uma rainha\n" ++ maxCells cellColumns queens
+   let s5 = ";cada diagonal possui no máximo uma rainha\n" ++ concat (map (\e -> maxCells e queens) diagonal)
+   let s6 = concat $ map (\e -> maxCells e queens) antidiagonal
+   writeFile (show queens ++ "queens.smt") $ s1 ++ "\n" ++ s2 ++ "\n" ++ s3 ++ "\n" ++ s4 ++ "\n" ++ s5 ++ s6 ++ "\n(check-sat)\n(get-model)"
