12dbb6e5de5680950873484a9589eea8d7addf47
helena <helenavargastannuri@gmail.com>
2024-10-17 19:54:37 -0300

    ta na mao

--- generate.hs
+++ generate.hs
@@ -1,14 +1,11 @@
-queens :: Int
-queens = 5
-
 atomify :: (Show a, Show b) => (a, b) -> String
-atomify (x, y) = "q" ++ show x ++ show y
+atomify (x, y) = "q" ++ show x ++ "_" ++ show y
 
 andify :: (Show a, Show b, Show c, Show d) => (a, b) -> (c, d) -> String
 andify a b = "(and " ++ atomify a ++ " " ++ atomify b ++ ")"
 
 getCons :: (Show a, Show b) => [(a, b)] -> String
-getCons l = concat (map (\(a, b) -> "(declare-const q" ++ show a ++ show b ++ " Bool)\n") l)
+getCons l = concat (map (\(a, b) -> "(declare-const q" ++ show a ++ "_" ++ show b ++ " Bool)\n") l)
 
 minCell :: (Show a, Show b) => [(a, b)] -> String
 minCell l = "(assert (or" ++ concat (map (\e -> " " ++ atomify e) l) ++ "))"
@@ -31,24 +28,27 @@ divider :: Int -> [a] -> [[a]]
 divider n [] = []
 divider n l = take n l : divider n (drop n l)
 
-cellLines :: [(Int, Int)]
-cellLines = [(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1]]
+cellLines :: Int -> [(Int, Int)]
+cellLines queens = [(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1]]
 
-cellColumns :: [(Int, Int)]
-cellColumns = [(j, i) | i <- [0 .. queens - 1], j <- [0 .. queens - 1]]
+cellColumns :: Int -> [(Int, Int)]
+cellColumns queens = [(j, i) | i <- [0 .. queens - 1], j <- [0 .. queens - 1]]
 
-diagonal :: [[(Int, Int)]]
-diagonal = [[(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1], i - j == k] | k <- [-queens + 2 .. queens - 2]]
+diagonal :: Int -> [[(Int, Int)]]
+diagonal queens = [[(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1], i - j == k] | k <- [-queens + 2 .. queens - 2]]
 
-antidiagonal :: [[(Int, Int)]]
-antidiagonal = [[(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1], i + j == k] | k <- [1 .. 2 * queens - 3]]
+antidiagonal :: Int -> [[(Int, Int)]]
+antidiagonal queens = [[(i, j) | i <- [0 .. queens - 1], j <- [0 .. queens - 1], i + j == k] | k <- [1 .. 2 * queens - 3]]
 
 main :: IO ()
 main = do
-   let s1 = ";constantes\n" ++ getCons cellLines
-   let s2 = ";cada linha possui ao menos uma rainha\n" ++ minCells (divider queens cellLines)
-   let s3 = ";cada linha possui no máximo uma rainha\n" ++ maxCells cellLines queens
-   let s4 = ";cada coluna possui no máximo uma rainha\n" ++ maxCells cellColumns queens
-   let s5 = ";cada diagonal possui no máximo uma rainha\n" ++ concat (map (\e -> maxCells e queens) diagonal)
-   let s6 = concat $ map (\e -> maxCells e queens) antidiagonal
-   writeFile (show queens ++ "queens.smt") $ s1 ++ "\n" ++ s2 ++ "\n" ++ s3 ++ "\n" ++ s4 ++ "\n" ++ s5 ++ s6 ++ "\n(check-sat)\n(get-model)"
+   putStrLn "Digite um número natural maior que 3"
+   s <- getLine
+   let n = read s
+   let s1 = ";constantes\n" ++ getCons (cellLines n)
+   let s2 = ";cada linha possui ao menos uma rainha\n" ++ minCells (divider n (cellLines n))
+   let s3 = ";cada linha possui no máximo uma rainha\n" ++ maxCells (cellLines n) n
+   let s4 = ";cada coluna possui no máximo uma rainha\n" ++ maxCells (cellColumns n) n
+   let s5 = ";cada diagonal possui no máximo uma rainha\n" ++ concat (map (\e -> maxCells e n) $ diagonal n)
+   let s6 = concat $ map (\e -> maxCells e n) $ antidiagonal n
+   writeFile (show n ++ "queens.smt") $ s1 ++ "\n" ++ s2 ++ "\n" ++ s3 ++ "\n" ++ s4 ++ "\n" ++ s5 ++ s6 ++ "\n(check-sat)\n(get-model)"
